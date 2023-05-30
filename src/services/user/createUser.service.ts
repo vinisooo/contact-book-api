daaa04922a04c9e1a8a5eeeb3b66437ff5f597ba
vinisooo <cviniciusliramdev@gmail.com>
2023-05-30 19:37:04 -0300

    fix: adjust request returns

--- src/services/user/createUser.service.ts
+++ src/services/user/createUser.service.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { AppDataSource } from "../../data-source";
 import { iUserReq, iNoPasswordUser } from "../../interfaces/user.interfaces";
 import { User } from "../../entities/users.entities";
-import { noPasswordUserSerializer } from "../../serializers/user.serializers";
+import { noPasswordNoContactsUserSerializer, noPasswordUserSerializer } from "../../serializers/user.serializers";
 import { Repository } from "typeorm";
 
 const createUserService = async(data: iUserReq) => {
@@ -11,7 +11,8 @@ const createUserService = async(data: iUserReq) => {
     const user = userRepository.create(data);
     await userRepository.save(user);
 
-    return user;
+    const serializedUser = noPasswordNoContactsUserSerializer.parse(user);
+    return serializedUser;
 }
 
 export default createUserService
--- src/services/user/updateUser.service.ts
+++ src/services/user/updateUser.service.ts
@@ -2,6 +2,7 @@ import { AppDataSource } from "../../data-source";
 import { User } from "../../entities/users.entities";
 import { iUserUpdate } from "../../interfaces/user.interfaces";
 import { Repository } from "typeorm";
+import { noPasswordNoContactsUserSerializer } from "../../serializers/user.serializers";
 
 const updateUserService = async(payload: object, oldData: User) => {
     const userRepository: Repository<User> = AppDataSource.getRepository(User);
@@ -13,7 +14,8 @@ const updateUserService = async(payload: object, oldData: User) => {
 
     await userRepository.save(updatedUser);
 
-    return updatedUser
+    const serializedUser = noPasswordNoContactsUserSerializer.parse(updatedUser);
+    return serializedUser;
 }
 
 export default updateUserService
